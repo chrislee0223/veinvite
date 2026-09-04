e714a0689177793d75d66b702c1ac855ca3b6d0e
chrislee0223 <ldh0223mm@gmail.com>
2026-09-04 18:10:25 +0900

    Load final invite mission rule copy

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -12,6 +12,7 @@ import '@/lib/i18n/localePacks/registerExpandedLocales';
 import '@/lib/i18n/inviteLandingFinalPolish';
 import '@/lib/i18n/copyHardening';
 import '@/lib/i18n/inviteeMissionCopyPolish';
+import '@/lib/i18n/inviteeMissionRulePolish';
 import '@/lib/i18n/guideCopyFinalHardening';
 import '@/lib/i18n/secondaryPageCopyHardening';
 import '@/lib/i18n/referralLinkCopy';
