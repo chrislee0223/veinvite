30a08cc6fe4bdf80532530441587f62c90367075
chrislee0223 <ldh0223mm@gmail.com>
2026-09-09 19:48:18 +0900

    Add Zoom Network QA v3 route

---
+++ src/app/qa/network-zoom-v3/page.tsx
@@ -0,0 +1,7 @@
+import { QaNetworkZoomV3 } from '@/qa/QaNetworkZoomV3';
+
+export const dynamic = 'force-dynamic';
+
+export default function QaNetworkZoomV3Page() {
+  return <QaNetworkZoomV3 />;
+}
