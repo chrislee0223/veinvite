2a2d92cf01a9dab7a030fbfced9f27abec5d4a94
chrislee0223 <ldh0223mm@gmail.com>
2026-09-10 10:15:02 +0900

    Add refined network empty-state QA v2 route

---
+++ src/app/qa/network-empty-state-v2/page.tsx
@@ -0,0 +1,7 @@
+import { QaNetworkEmptyStateV2 } from '@/qa/QaNetworkEmptyStateV2';
+
+export const dynamic = 'force-dynamic';
+
+export default function NetworkEmptyStateV2Page() {
+  return <QaNetworkEmptyStateV2 />;
+}
