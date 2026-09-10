2dfea563a4b74bd59dc745bde34879fd34e05fdd
chrislee0223 <ldh0223mm@gmail.com>
2026-09-10 11:11:39 +0900

    Add network empty-state QA v5 route

---
+++ src/app/qa/network-empty-state-v5/page.tsx
@@ -0,0 +1,7 @@
+import { QaNetworkEmptyStateV5 } from '@/qa/QaNetworkEmptyStateV5';
+
+export const dynamic = 'force-dynamic';
+
+export default function NetworkEmptyStateV5Page() {
+  return <QaNetworkEmptyStateV5 />;
+}
