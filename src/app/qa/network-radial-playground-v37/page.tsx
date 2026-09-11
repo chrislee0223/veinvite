dfb2cbf11b2f7cd22bc5a6dbb5c3f063b3c5d634
chrislee0223 <ldh0223mm@gmail.com>
2026-09-11 17:33:05 +0900

    Add radial network playground v37 route

---
+++ src/app/qa/network-radial-playground-v37/page.tsx
@@ -0,0 +1,5 @@
+import { QaNetworkRadialPlaygroundV37 } from '@/qa/QaNetworkRadialPlaygroundV37';
+
+export default function NetworkRadialPlaygroundV37Page() {
+  return <QaNetworkRadialPlaygroundV37 />;
+}
