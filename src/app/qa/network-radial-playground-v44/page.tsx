a69f62a7bfa6facc75cd44c48079d42c466246a3
chrislee0223 <ldh0223mm@gmail.com>
2026-09-11 23:59:31 +0900

    Add V44 group creation and dismiss UX

---
+++ src/app/qa/network-radial-playground-v44/page.tsx
@@ -0,0 +1,5 @@
+import { QaNetworkRadialPlaygroundV44 } from '@/qa/QaNetworkRadialPlaygroundV44';
+
+export default function NetworkRadialPlaygroundV44Page() {
+  return <QaNetworkRadialPlaygroundV44 />;
+}
