7212c92a08765e4fd0449544e0bf51ca0902cea0
chrislee0223 <ldh0223mm@gmail.com>
2026-09-11 20:59:07 +0900

    Retry V37 QA preview deployment

--- src/app/qa/network-radial-playground-v37/page.tsx
+++ src/app/qa/network-radial-playground-v37/page.tsx
@@ -4,4 +4,4 @@ export default function NetworkRadialPlaygroundV37Page() {
   return <QaNetworkRadialPlaygroundV37 />;
 }
 
-// QA route marker: infinite-canvas v37 ready
+// QA route marker: infinite-canvas v37 redeploy-check
