4e818aecde090b54e8237c71d4764c1341628279
chrislee0223 <ldh0223mm@gmail.com>
2026-09-12 00:20:43 +0900

    Add V45 stability and rendering safeguards

---
+++ src/app/qa/network-radial-playground-v45/page.tsx
@@ -0,0 +1,5 @@
+import { QaNetworkRadialPlaygroundV45 } from '@/qa/QaNetworkRadialPlaygroundV45';
+
+export default function NetworkRadialPlaygroundV45Page() {
+  return <QaNetworkRadialPlaygroundV45 />;
+}
