7d381fc9586f79f7d37509f8eb6ae7dee8a44874
chrislee0223 <ldh0223mm@gmail.com>
2026-09-11 22:48:48 +0900

    Add V40 manual groups preview route

---
+++ src/app/qa/network-radial-playground-v40/page.tsx
@@ -0,0 +1,5 @@
+import { QaNetworkRadialPlaygroundV40 } from '@/qa/QaNetworkRadialPlaygroundV40';
+
+export default function NetworkRadialPlaygroundV40Page() {
+  return <QaNetworkRadialPlaygroundV40 />;
+}
