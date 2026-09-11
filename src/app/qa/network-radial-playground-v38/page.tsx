f0c9575f780714878ea2eecf47eeb97f1c9bab7b
chrislee0223 <ldh0223mm@gmail.com>
2026-09-11 21:34:56 +0900

    Add V38 network stability pass

---
+++ src/app/qa/network-radial-playground-v38/page.tsx
@@ -0,0 +1,5 @@
+import { QaNetworkRadialPlaygroundV38 } from '@/qa/QaNetworkRadialPlaygroundV38';
+
+export default function NetworkRadialPlaygroundV38Page() {
+  return <QaNetworkRadialPlaygroundV38 />;
+}
