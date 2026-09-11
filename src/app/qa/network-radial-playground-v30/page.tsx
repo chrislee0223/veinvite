f0b2547e8bd78c6e5892a567c56155c969db70aa
chrislee0223 <ldh0223mm@gmail.com>
2026-09-11 13:56:53 +0900

    Add radial network playground v30 route

---
+++ src/app/qa/network-radial-playground-v30/page.tsx
@@ -0,0 +1,7 @@
+import { QaNetworkRadialPlaygroundV30 } from '@/qa/QaNetworkRadialPlaygroundV30';
+
+export const dynamic = 'force-dynamic';
+
+export default function QaNetworkRadialPlaygroundV30Page() {
+  return <QaNetworkRadialPlaygroundV30 />;
+}
