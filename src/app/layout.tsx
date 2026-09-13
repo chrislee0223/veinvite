cf340e60de73396692b15b596df76fe4e31d6e3c
chrislee0223 <ldh0223mm@gmail.com>
2026-09-13 17:41:33 +0900

    Load shared mobile-style tab width guard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { LocaleDocumentSync } from '@/components/LocaleDocumentSync';
 import { LocaleHydrationShield } from '@/components/LocaleHydrationShield';
 import { UsageAnalyticsTracker } from '@/components/UsageAnalyticsTracker';
 import './globals.css';
+import './mobile-shell-width-unification.css';
 import './header-language-flags.css';
 import './localized-typography.css';
 import './notification-i18n-hardening.css';
